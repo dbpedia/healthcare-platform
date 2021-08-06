e98bfa05e4a441daa381191191d53c3fb60d7828
guang-zh <guang.zhang@dawsoncollege.qc.ca>
2021-08-06 00:22:44 -0400

    edited dbpedia_databus_upload.sh

--- Scripts/dbpedia_databus_upload.sh
+++ Scripts/dbpedia_databus_upload.sh
@@ -1,21 +1,30 @@
 # DBpedia Server and Databus upload
 
-#$ nano config
+nano config
 # add DBpedia as host
-
-#$ssh dbpedia
-#$tmux a
-
+Host gfs
+	Hostname dbpedia.informatik.uni-leipzig.de
+	User root
+	Port 22
+ssh dbpedia
+tmux a
+# co-editing and demo for ssh dbpedia session
 # ctrl+b d for exiting session
 
 
+## add to databus
+# Link to upload on DBpedia server: https://databus.dbpedia.org/system/upload
+# Online upload doesn't work
 
-## add databus
 
-#$ nano pom.xml
+# Upload using Maven Deploy
+git clone https://github.com/dbpedia/databus-maven-plugin
+
+nano pom.xml
 # change groupID to "healthcare"
 # change version to date
 # module "covid-world-vaccination-progress"
 # change databus member to "guang"
 
 # create webID authentication
+# Obtain Webid: https://github.com/dbpedia/webid
