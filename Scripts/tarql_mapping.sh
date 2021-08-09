c1ad72e3cdbda65edbad78a0d0a9184a70cea2dc
guang-zh <guang.zhang@dawsoncollege.qc.ca>
2021-08-09 10:42:25 -0400

    added covid-19 tracking germany for covid data set

--- Scripts/tarql_mapping.sh
+++ Scripts/tarql_mapping.sh
@@ -7,6 +7,9 @@ pwd
 ../../../tarql/bin/tarql --ntriples country_vaccinations_sparql.sparql country_vaccinations.csv > country_vaccinations_triples_rerun.nt
 ../../../tarql/bin/tarql --ntriples country_vaccinations_by_manufacturer_sparql.sparql country_vaccinations_by_manufacturer.csv > country_vaccinations_by_manufacturer_triples.nt
 
+# mapping for Covid 19 Tracking Germany
+../../tarql/bin/tarql --ntriples covid_de_vaccines_sparql.sparql covid_de_vaccines.csv > covid_de_vaccines_triples.nt
+../../tarql/bin/tarql --ntriples covid_de_sparql.sparql covid_de.csv > covid_de_triples.nt
 
 # compress files with bzip2
 bzip2 country_vaccinations.csv
